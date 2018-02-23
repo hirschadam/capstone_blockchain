021ecb4421dcfd74b738effef087591656e799c2
Adam Hirsch <aah1619@rit.edu>
2018-02-23 13:52:03 -0500

    fixed

--- configurations.py
+++ configurations.py
@@ -2,13 +2,13 @@ class Load_Configuation:
     # 
     vars={
         "port":["str"],
-        "genesis_hash":["str"]
-        "node_ip":["str"]
-        "version":["str"]
-        "whitelist":["list"]
-        "blacklist"["list"]
-        "show_address":["str"]
-        "accept_peers":["str"]
+        "genesis_hash":["str"],
+        "node_ip":["str"],
+        "version":["str"],
+        "whitelist":["list"],
+        "blacklist"["list"],
+        "show_address":["str"],
+        "accept_peers":["str"],
     }
 
     def read_default(self):
