35f3239c721909f7ce185cc89a37d2f84c2b749d
avj2668 <avj2668@g.rit.edu>
2018-03-19 13:19:43 -0400

    added code to open config.txt and peers.txt

--- node.py
+++ node.py
@@ -11,5 +11,29 @@ import socket
 class Node:
 	
 	def __init__(self, nodeId, name, ip, listenPort, sendingPort):
-		with open('config.txt') as f:
-			lines = 
+		pass
+	
+
+def main():
+	
+	filename = 'config.txt'
+	peers_file = ''
+	f = open(filename)
+	for row in f:
+		line = row.split('=')
+		if (line[0] == 'neighbors'):
+			peers_file = line[1].rstrip('\n')
+	
+	f = open(peers_file)
+	header = True
+	for neighbor in f:
+		if header:
+			header = False
+		else:
+			(idx, name, ip_addr, port_send, port_recv) = neighbor.split(',')
+			print(idx,name)
+
+main()
+	
+
+
