740356aee520576f99b47147fdfa1018a7af6a57
Me <rfrank2727@gmail.com>
2018-03-19 12:51:06 -0400

    add to node class

--- node.py
+++ node.py
@@ -1,1 +1,15 @@
-# Initial commit
+# Planning
+# 	1 set up node listening on a socket
+#		1.1 setup config
+#			1.1.1 list of neighbors
+#			1.1.2 default port
+#	2 transmit data from one node to another
+
+import socket
+
+
+class Node:
+	
+	def __init__(self, nodeId, name, ip, listenPort, sendingPort):
+		with open('config.txt') as f:
+			lines = 
