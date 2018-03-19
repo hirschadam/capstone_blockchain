a508c33478f4ed79e05fd5ddaa125533e67d5df3
Me <rfrank2727@gmail.com>
2018-03-19 13:21:51 -0400

    add some functions that we'll need later

---
+++ nodeSkeleton.py
@@ -0,0 +1,17 @@
+# delete this later
+class Node:
+	def __init__(self, config):
+	# fuctions for peer managment
+	def getInitialNeighbors(config):
+	def listNeighbors():
+	def addNeighbor(nodeId, name, ip_addr, port_send, port_recv):
+	def blacklistNeighbor(nodeId):
+	# fuctions for sending and reciving transactions and blocks
+	def sendTransaction():
+	def verifyTransaction():
+	def sendBlock():
+	def verifyBlock():
+	#functions for managing the blockchain
+	def listBlocks():
+	def addToBlockchain():
+	def getNewestBlock():
