221827380983ddb10a48d3c234e39ceee4a123ce
Nicholas O'Brien <ndo9903@rit.edu>
2018-03-21 18:53:54 -0400

    moved around some stuff between blokc and blockchain

--- block.py
+++ block.py
@@ -3,73 +3,28 @@ import time
 import hashlib
 
 class Block:
-    def __init__(self, index, prevHash, timestamp, data, currHash):
+    def __init__(self, index, prevHash, timestamp, data):
         self.index = index
         self.prevHash = prevHash
         self.timestamp = timestamp
         self.data = data
-        self.currHash = currHash
+        self.currHash = self.calculateHash()
 
-    def getGenesisBlock():
-        return Block(0, '0', 1518809761.5113006, "kek", "kek3st4nil4nd0000000000")
-
-    blockchain = [getGenesisBlock()]
-
-    def calculateHash(index, prevHash, timestamp, data):
-        value = str(index) + str(prevHash) + str(timestamp) + str(data)
+    def calculateHash(self):
+        value = str(self.index) + str(self.prevHash) + str(self.timestamp) + str(self.data)
         sha = hashlib.sha256(value.encode('utf-8'))
-
         return str(sha.hexdigest())
 
-    def calculateHashForBlock(block):
-        return calculateHash(block.index, block.prevHash, block.timestamp, block.data)
 
-    def getLatestBlock():
-        return blockchain[len(blockchain-1)]
-
-    def genNextBlock(blockData):
-        prevBlock = getLatestBlock()
-        nextIndex = prevBlock.index+1
-        nextTimestamp = time.time()
-        nextHash = calculateHash(nextIndex, prevBlock.currHash, nextTimestamp, blockData)
-        return Block(nextIndex, prevBlock.currHash, nextTimestamp, nextHash)
-
-    def isSameBlock(block1, block2):
-        if block1.index != block2.index:
+    def __eq__(self, other):
+        if self.index != other.index:
             return False
-        elif block1.prevHash != block2.prevHash:
+        if self.prevHash != other.prevHash:
             return False
-        elif block1.timestamp != block2.timestamp:
+        if self.timestamp != other.timestamp:
             return False
-        elif block1.data != block2.data:
+        if self.data != other.data:
             return False
-        elif block1.currHash != block2.currHash:
+        if self.currHash != other.currHash:
             return False
         return True
-
-    def isValidNewBlock(newBlock, prevBlock):
-        if prevBlock.index+1 != newBlock.index:
-            print('Indices Do Not Match Up')
-            return False
-        elif prevBlock.currHash != newBlock.prevHash:
-            print("Previous hash does not match")
-            return False
-        elif calculateHashForBlock(newBlock) != newBlock.hash:
-            print("Invalid hashpointer")
-            return False
-        return True
-            
-    def isValidChain(chain)
-        if not isSameBlock(chain[0], getGenesisBlock()):
-            print('Genesis Block Incorrect')
-            return False
-
-        tempBlocks = [chain[0]]
-        for i in range(1, len(chain)):
-            if isValidNewBlock(chain[i], tempBlocks[i-1]):
-                tempBlocks.append(chain[i])
-            else:
-                return False
-        return True
-
-
---
+++ blockchain.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+from block import Block
+
+class Blockchain:
+    def __init__(self):
+        self.chain = {}
+        self.genesisBlock = Block(0, '0', 1518809761.5113006, "kek")
+        self.chain[genesisBlock.currHash] = self.genesisBlock
+        self.tailBlockHash = genesisBlock.currHash
+
+    def addBlock(self, block):
+        if self.isValidBlock(block):
+            self.chain[block.currHash] = block
+            self.tailBlockHash = block.currHash
+            return True
+        return False
+
+    def getBlock(self, hash):
+        return self.chain[hash]
+
+    def calculateHashForBlock(block):
+        return block.calculateHash()
+
+    def isValidBlock(self, block):
+        prevBlock = self.getBlock(self.block.prevHash)
+        if prevBlock.index+1 != block.index:
+            print('Indices Do Not Match Up')
+            return False
+        elif prevBlock.currHash != block.prevHash:
+            print("Previous hash does not match")
+            return False
+        elif block.calculateHash() != block.currHash:
+            print("Invalid hashpointer")
+            return False
+        return block.isValid()
+
+    def isValid(self):
+        currBlock = self.getBlock(self.tailBlockHash)
+        while currBlock != self.genesisBlock:
+            if not self.isValidBlock(currBlock):
+                return False
+            currBlock = self.getBlock(currBlock.prevHash)
+        return True
