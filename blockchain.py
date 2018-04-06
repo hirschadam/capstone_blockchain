3e251cab7c134e27a9c3b7b4235876b6c4064379
Nicholas O'Brien <ndo9903@rit.edu>
2018-04-06 00:36:12 -0400

    added Docs for Blockchain

--- blockchain.py
+++ blockchain.py
@@ -4,12 +4,22 @@ from block import Block
 
 class Blockchain:
     def __init__(self):
+        """
+        Constructor for Blockchain
+        """
         self.chain = {}
         self.genesisBlock = Block(0, 'kek', 1518809761.5113006)
         self.chain[genesisBlock.currHash] = self.genesisBlock
         self.tailBlockHash = genesisBlock.currHash
 
     def addBlock(self, block):
+        """
+        Add Block to Blockchain
+        
+        @param block - Block to be added to the chain
+        
+        @return Boolean - True if block was added, False otherwise
+        """
         if self.isValidBlock(block):
             self.chain[block.currHash] = block
             self.tailBlockHash = block.currHash
@@ -17,9 +27,25 @@ class Blockchain:
         return False
 
     def getBlock(self, hash):
-        return self.chain[hash]
+        """
+        Get Block from Blockchain
+        
+        @param hash - Hash of Block to get
+        
+        @return Block - The Block in question, or None
+        """
+        if hash in self.chain:
+            return self.chain[hash]
+        return None
 
     def isValidBlock(self, block):
+        """
+        Checks if Block is Valid
+        
+        @param block - Block to be checked
+        
+        @return Boolean - True if block is valid, False otherwise
+        """
         prevBlock = self.getBlock(self.block.prevHash)
         if prevBlock.index+1 != block.index:
             print('Indices Do Not Match Up')
@@ -33,6 +59,11 @@ class Blockchain:
         return block.isValid()
 
     def isValid(self):
+        """
+        Check if Blockchain is Valid
+                
+        @return Boolean - True if Blockchain is Valid, False otherwise
+        """
         currBlock = self.getBlock(self.tailBlockHash)
         while currBlock != self.genesisBlock:
             if not self.isValidBlock(currBlock):
