f0042908839286eb33f04185841b6fa9394a2d9f
Nicholas O'Brien <ndo9903@rit.edu>
2018-04-06 00:32:38 -0400

    added Docs for Block

--- block.py
+++ block.py
@@ -4,6 +4,13 @@ import hashlib
 
 class Block:
     def __init__(self, index, prevHash, timestamp):
+        """
+        Constructor for Block
+        
+        @param index - Index in Chain
+        @param prevHash - Hash of previous block in chain
+        @param timestamp - Timestamp for when mining occurred
+        """
         self.index = index
         self.prevHash = prevHash
         self.timestamp = timestamp
@@ -12,6 +19,11 @@ class Block:
         self.currHash = self.calculateHash()
 
     def calculateHash(self):
+        """
+        Calculate Hash for Block
+        
+        @return Digest - Hash of the Block
+        """
         value = str(self.index) + str(self.prevHash) + str(self.timestamp) + str(self.nonce)
         for transaction in self.transactions:
             value = transaction.getDataString
@@ -20,18 +32,40 @@ class Block:
 
 
     def addTransaction(self, transaction, unSpentTransactions):
+        """
+        Add Transaction to Block
+        
+        @param transaction - New TX to Add
+        @param unSpentTransactions - Dictionary of TXs with unused Outputs
+        
+        @return Boolean - True if Transaction added, False otherwise
+        """
         if transaction.isValid(unSpentTransactions):
             self.transactions[transaction.hash] = transaction
             return True
         return False
 
     def removeTransaction(self, transaction):
+        """
+        Remove Transaction from Block
+        
+        @param transaction - TX to Remove
+        
+        @return Boolean - True if Transaction removed, False otherwise
+        """
         if transaction.hash in self.transactions:
             del self.transactions[transaction.hash]
             return True
         return False
 
     def isValid(self, unSpentTransactions):
+        """
+        Check if Block is Valid
+        
+        @param unSpentTransactions - Dictionary of TXs with unused Outputs
+        
+        @return Boolean - True if Block valid, False otherwise
+        """
         rewardCount = 0
         for transaction in self.transactions:
             if not transaction.isValid(unSpentTransactions):
@@ -45,6 +79,13 @@ class Block:
 
 
     def __eq__(self, other):
+        """
+        Overrides the "=" operator for Block
+        
+        @param other - Other Block to compare to
+        
+        @return Boolean - True if Block is equal, False otherwise
+        """
         if self.index != other.index:
             return False
         if self.prevHash != other.prevHash:
