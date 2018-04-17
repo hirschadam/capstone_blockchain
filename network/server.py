f89b870461880bcd46fe05fe0b4cc6f01052cfc9
avj2668 <avj2668@g.rit.edu>
2018-04-17 18:00:46 -0400

    created server.py and updated node.py

---
+++ network/server.py
@@ -0,0 +1,59 @@
+
+
+# Message types for communication between nodes
+REQUEST_NEIGHBORS = 1
+REPLY_NEIGHBORS = 2
+TRANSACTION = 3
+BLOCK = 4
+REQUEST_BLOCKCHAIN = 5
+REPLY_BLOCKCHAIN = 6
+
+class Server(Thread):
+
+	def __init__(self, node):
+
+		Thread.__init__(self)
+		self.port = node.port_recv
+		self.host = node.get_ip_addr()
+		self.name = node.get_name()
+		self.bufsize = 1024
+		self.addr = (self.host, self.port)
+		self.node_id = node.get_id()
+		self.node = node
+
+		self.socket = socket(AF_INET , SOCK_STREAM)
+		self.socket.bind(self.addr)
+
+	def run(self):
+		self.socket.listen(5)           
+		while True:
+            print('Waiting for connection..')
+	    	client, caddr = self.socket.accept()
+		    print('Connected To', caddr)
+
+			serializedData = client.recv(self.bufsize)
+			data = pickle.loads(serializedData)
+			if data:
+			    messageType, payload = data
+			    if messageType == REQUEST_NEIGHBORS:
+				    self.node.sendData((REPLY_NEIGHBORS, self.node.peer_list), payload)
+			    if messageType == REPLY_NEIGHBORS:
+				    newPeers = set(self.node.peer_list)
+				    newPeers.update(payload)
+				    self.node.peer_list = list(newPeers)
+			    if messageType == TRANSACTION:
+				    self.node.unspentTransactions[payload.hash] = payload
+				    self.node.currBlock.addTransaction(payload, self.node.unspentTransactions)
+			    if messageType == BLOCK:
+				    if self.blockChain.isValidBlock(payload):
+					    for peer in self.node.get_peer_list():
+						    self.sendData((4, payload), peer)
+						    self.blockChain.addBlock(payload)
+					    self.currBlock = Block(payload.index, payload.currHash, datetime.datetime.utcnow().__str__())
+
+                client.close()
+			else:
+                client.close()
+			    continue
+
+
