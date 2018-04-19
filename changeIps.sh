f82c3bbda97850d63e00d5a14844307ab90a1c67
Me <rfrank2727@gmail.com>
2018-04-19 18:00:38 -0400

    ifconfig for scrubs

--- changeIps.sh
+++ changeIps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-newIp="$(ip a | grep 129.21..*/ -o | sed 's/\///')"
+newIp="$(ifconfig | grep 129.21..*Bc -o | sed 's/Bc//')"
 oldIp="$(cat node1.txt | grep 129.21..*$ -o)"
 
-sed -ie "s/$oldIp/$newIp/g" *.txt
-rm *.txte
+sed -i "s/$oldIp/$newIp/g" *.txt
+rm *.txte 2>/dev/null
